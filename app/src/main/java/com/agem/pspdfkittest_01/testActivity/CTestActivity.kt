c92215912e6ea733d152343be2d54fea9daa44d3
dqwdd-mega <dqwdd@megastudy.net>
2023-09-06 09:43:43 +0900

    [Feat] : 툴바 관련 삭제했는데 onDocumentLongPress 안먹힘
    Commit Date : 2023-09-06(수)

--- app/src/main/java/com/agem/pspdfkittest_01/testActivity/CTestActivity.kt
+++ app/src/main/java/com/agem/pspdfkittest_01/testActivity/CTestActivity.kt
@@ -10,7 +10,6 @@ import android.util.Log
 import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
@@ -31,20 +30,6 @@ import com.pspdfkit.listeners.DocumentListener
 import com.pspdfkit.listeners.OnDocumentLongPressListener
 import com.pspdfkit.listeners.SimpleDocumentListener
 import com.pspdfkit.ui.PdfFragment
-import com.pspdfkit.ui.inspector.PropertyInspectorCoordinatorLayout
-import com.pspdfkit.ui.inspector.annotation.AnnotationCreationInspectorController
-import com.pspdfkit.ui.inspector.annotation.AnnotationEditingInspectorController
-import com.pspdfkit.ui.inspector.annotation.DefaultAnnotationCreationInspectorController
-import com.pspdfkit.ui.inspector.annotation.DefaultAnnotationEditingInspectorController
-import com.pspdfkit.ui.special_mode.controller.AnnotationCreationController
-import com.pspdfkit.ui.special_mode.controller.AnnotationEditingController
-import com.pspdfkit.ui.special_mode.controller.TextSelectionController
-import com.pspdfkit.ui.special_mode.manager.AnnotationManager
-import com.pspdfkit.ui.special_mode.manager.TextSelectionManager
-import com.pspdfkit.ui.toolbar.AnnotationCreationToolbar
-import com.pspdfkit.ui.toolbar.AnnotationEditingToolbar
-import com.pspdfkit.ui.toolbar.TextSelectionToolbar
-import com.pspdfkit.ui.toolbar.ToolbarCoordinatorLayout
 import io.reactivex.rxjava3.disposables.Disposable
 import java.util.*
 import kotlin.math.min
@@ -53,33 +38,13 @@ import kotlin.math.min
  * 이 Activity 는 화면 Split 하게 나오는 거 테스트하는 Activity
  * */
 
-class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(
-    R.layout.activity_ctest),
-    AnnotationManager.OnAnnotationCreationModeChangeListener,
-    AnnotationManager.OnAnnotationEditingModeChangeListener,
-    TextSelectionManager.OnTextSelectionModeChangeListener,
-    DocumentListener,
-    OnDocumentLongPressListener{
+class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(R.layout.activity_ctest),
+    DocumentListener, OnDocumentLongPressListener {
 
     private val viewModel: CTestViewModel by viewModels()
 
     private lateinit var mFragment: PdfFragment
-
-    private var annotationToolbarCreationActive = false
-
     private var mBitmap: Bitmap? = null
-
-    // 툴바 열기 위한 것들
-    private lateinit var annotationCreationButton: Button
-    private lateinit var toolbarCoordinatorLayout: ToolbarCoordinatorLayout
-
-    private lateinit var annotationCreationToolbar: AnnotationCreationToolbar
-    private lateinit var textSelectionToolbar: TextSelectionToolbar
-    private lateinit var annotationEditingToolbar: AnnotationEditingToolbar
-
-    private lateinit var inspectorCoordinatorLayout: PropertyInspectorCoordinatorLayout
-    private lateinit var annotationEditingInspectorController: AnnotationEditingInspectorController
-    private lateinit var annotationCreationInspectorController: AnnotationCreationInspectorController
 
     // 주석 값 저장하는 것들
     private val documentAnnotations = mutableListOf<Annotation>()
@@ -90,7 +55,6 @@ class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setToolbar()
         setupEvent()
     }
 
@@ -120,45 +84,13 @@ class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(
             }
             return@setOnTouchListener true
         }
+
         binding.btnGetAnnotationLocateData.setOnClickListener {
             getAnnotationLocateData()
         }
-
         binding.btnRenderAnnotation.setOnClickListener {
             renderAnnotation()
         }
-    }
-
-    private fun setToolbar() {
-        toolbarCoordinatorLayout = findViewById(R.id.toolbarCoordinatorLayout)
-
-        annotationCreationToolbar = AnnotationCreationToolbar(this)
-        textSelectionToolbar = TextSelectionToolbar(this)
-        annotationEditingToolbar = AnnotationEditingToolbar(this)
-
-        // Use this if you want to use annotation inspector with annotation creation and editing toolbars.
-        inspectorCoordinatorLayout = findViewById(R.id.inspectorCoordinatorLayout)
-        annotationEditingInspectorController = DefaultAnnotationEditingInspectorController(this, inspectorCoordinatorLayout)
-        annotationCreationInspectorController = DefaultAnnotationCreationInspectorController(this, inspectorCoordinatorLayout)
-
-    }
-
-    private fun initAnnotationCreationButton() {
-        annotationCreationButton = findViewById(R.id.openAnnotationEditing)
-        annotationCreationButton.setOnClickListener {
-            if (annotationToolbarCreationActive) {
-                mFragment.exitCurrentlyActiveMode()
-            } else {
-                mFragment.enterAnnotationCreationMode()
-            }
-        }
-
-        updateButtonText()
-    }
-
-    private fun updateButtonText() {
-        val tt = if (annotationToolbarCreationActive) "close_editor" else "open_editor"
-        binding.openAnnotationEditing.text = tt
     }
 
     private fun showOpenFileDialog() {
@@ -167,7 +99,6 @@ class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(
         intent.putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("application/pdf", "image/*"))
         requestPDF.launch(intent)
     }
-
     // https://pspdfkit.com/guides/android/customizing-the-interface/using-toolbars-within-fragment/
 
     @SuppressLint("ResourceType")
@@ -187,29 +118,25 @@ class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(
                     return@run newFragment
                 }
 
-
-            // 툴바 리스너 apply
             mFragment.apply {
-                addOnAnnotationCreationModeChangeListener(this@CTestActivity)
-                addOnAnnotationEditingModeChangeListener(this@CTestActivity)
-                addOnTextSelectionModeChangeListener(this@CTestActivity)
+                setOnDocumentLongPressListener(this@CTestActivity)
                 addDocumentListener(object : SimpleDocumentListener() {
                     @UiThread
                     override fun onDocumentLoaded(loadedDocument: PdfDocument) {
                         annotationLoadingDisposable = loadedDocument.annotationProvider
                             .getAllAnnotationsOfTypeAsync(EnumSet.allOf(AnnotationType::class.java))
                             .toList()
-                            .subscribe { annotations -> documentAnnotations.addAll(annotations) }
+                            .subscribe { annotations ->
+                                documentAnnotations.addAll(annotations)
+                                getAnnotationLocateData()
+                            }
                     }
 
                     override fun onDocumentLoadFailed(exception: Throwable) {
                         Log.e("tetest", "onDocumentLoadFailed 222255543444")
                     }
                 })
-                setOnDocumentLongPressListener(this@CTestActivity)
             }
-
-            initAnnotationCreationButton()
         }
     }
 
@@ -337,80 +264,7 @@ class CTestActivity : BaseBindingActivity<ActivityCtestBinding>(
             val action = longPressedAnnotation.boundingBox
         }
 
-        return true
-    }
-
-
-
-    // 아래는 툴바 관련
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mFragment.removeOnAnnotationCreationModeChangeListener(this)
-        mFragment.removeOnAnnotationEditingModeChangeListener(this)
-        mFragment.removeOnTextSelectionModeChangeListener(this)
-    }
-
-    // AnnotationManager.OnAnnotationCreationModeChangeListener
-    override fun onEnterAnnotationCreationMode(controller: AnnotationCreationController) {
-        annotationCreationInspectorController.bindAnnotationCreationController(controller)
-
-        annotationCreationToolbar.bindController(controller)
-        toolbarCoordinatorLayout.displayContextualToolbar(annotationCreationToolbar, true)
-        annotationToolbarCreationActive = true
-        updateButtonText()
-    }
-
-    // AnnotationManager.OnAnnotationCreationModeChangeListener
-    override fun onChangeAnnotationCreationMode(controller: AnnotationCreationController) {
-        // Nothing to be done here,
-        // if toolbar is bound to the controller it will pick up the
-        // changes.
-    }
-
-    // AnnotationManager.OnAnnotationCreationModeChangeListener
-    override fun onExitAnnotationCreationMode(controller: AnnotationCreationController) {
-        toolbarCoordinatorLayout.removeContextualToolbar(true)
-        annotationCreationToolbar.unbindController()
-        annotationToolbarCreationActive = false
-
-        annotationCreationInspectorController.unbindAnnotationCreationController()
-        updateButtonText()
-
-    }
-
-    // AnnotationManager.OnAnnotationEditingModeChangeListener
-    override fun onEnterAnnotationEditingMode(controller: AnnotationEditingController) {
-        annotationEditingInspectorController.bindAnnotationEditingController(controller)
-
-        annotationEditingToolbar.bindController(controller)
-        toolbarCoordinatorLayout.displayContextualToolbar(annotationEditingToolbar, true)
-    }
-
-    // AnnotationManager.OnAnnotationEditingModeChangeListener
-    override fun onChangeAnnotationEditingMode(controller: AnnotationEditingController) {
-        // Nothing to be done here, if toolbar is bound to the controller it will pick up the
-        // changes.
-    }
-
-    // AnnotationManager.OnAnnotationEditingModeChangeListener
-    override fun onExitAnnotationEditingMode(controller: AnnotationEditingController) {
-        toolbarCoordinatorLayout.removeContextualToolbar(true)
-        annotationEditingToolbar.unbindController()
-
-        annotationEditingInspectorController.unbindAnnotationEditingController()
-    }
-
-    // TextSelectionManager.OnTextSelectionModeChangeListener
-    override fun onEnterTextSelectionMode(controller: TextSelectionController) {
-        textSelectionToolbar.bindController(controller)
-        toolbarCoordinatorLayout.displayContextualToolbar(textSelectionToolbar, true)
-    }
-
-    // TextSelectionManager.OnTextSelectionModeChangeListener
-    override fun onExitTextSelectionMode(controller: TextSelectionController) {
-        toolbarCoordinatorLayout.removeContextualToolbar(true)
-        textSelectionToolbar.unbindController()
+        return false
     }
 
 }
